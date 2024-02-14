25caf7488fcbb29d8d2db4fc132de3a54b4c874b
Gaby Nieva <gaby.nieva@outlook.com>
2024-02-14 13:31:26 -0600

    Adding the ListView to the HomeScreen

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -7,6 +7,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
