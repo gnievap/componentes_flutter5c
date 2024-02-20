941e8f252a36426079f5c184aa105f93a67a4acf
Gaby Nieva <gaby.nieva@outlook.com>
2024-02-20 13:41:35 -0600

    Finalizing the app theme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3_5c24/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,35 +13,56 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            leading: const Icon(Icons.input_rounded),
+            leading: const Icon(
+              Icons.input_rounded,
+              color: AppTheme.mainColor,
+            ),
             title: Text(
               'Entradas',
-              style: Theme.of(context).textTheme.headlineLarge,
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
             ),
-            subtitle: const Text(
+            subtitle: Text(
               'Diferentes widgets para entradas de flutter',
+              style: AppTheme.lightTheme.textTheme.bodySmall,
             ),
-            trailing: const Icon(Icons.arrow_circle_right),
+            trailing: const Icon(
+              Icons.arrow_circle_right,
+              color: AppTheme.mainColor,
+            ),
           ),
           const Divider(),
           ListTile(
-            leading: const Icon(Icons.list_alt_rounded),
-            title: Text(
-              'ListView.builder',
-              style: Theme.of(context).textTheme.headlineLarge,
+            leading: const Icon(
+              Icons.list_alt_rounded,
+              color: AppTheme.mainColor,
             ),
-            subtitle: const Text('Scroll infinito'),
-            trailing: const Icon(Icons.arrow_circle_right),
+            title: Text('ListView.builder',
+                style: AppTheme.lightTheme.textTheme.headlineLarge),
+            subtitle: Text(
+              'Scroll infinito',
+              style: AppTheme.lightTheme.textTheme.bodySmall,
+            ),
+            trailing: const Icon(
+              Icons.arrow_circle_right,
+              color: AppTheme.mainColor,
+            ),
           ),
           const Divider(),
           ListTile(
-            leading: const Icon(Icons.notification_add),
-            title: Text(
-              'Notificaciones',
-              style: Theme.of(context).textTheme.headlineLarge,
+            leading: const Icon(
+              Icons.notification_add,
+              color: AppTheme.mainColor,
             ),
-            subtitle: const Text('Creación de notificaciones'),
-            trailing: const Icon(Icons.arrow_circle_right),
+            title: Text('Notificaciones',
+                style: AppTheme.lightTheme.textTheme.headlineLarge),
+            subtitle: Text(
+              'Creación de notificaciones',
+              style: AppTheme.lightTheme.textTheme.bodySmall,
+            ),
+            trailing: const Icon(
+              Icons.arrow_circle_right,
+              color: AppTheme.mainColor,
+            ),
           ),
         ],
       ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   // Declaraciones de color
   static const mainColor = Color.fromARGB(255, 4, 4, 61);
+  static const accentColor = Color.fromARGB(255, 33, 41, 112);
   static const backColor = Color.fromARGB(255, 222, 231, 244);
 
   // Declaraciones del tema claro
@@ -17,11 +18,17 @@ class AppTheme {
         fontWeight: FontWeight.w500,
         decoration: TextDecoration.underline,
         decorationColor: mainColor,
-        decorationStyle: TextDecorationStyle.wavy,
-        decorationThickness: 3.0,
+        decorationStyle: TextDecorationStyle.double,
+        decorationThickness: 1.5,
         fontStyle: FontStyle.italic,
       ),
-      //bodySmall:
+      //Textos muy pequeños
+      bodySmall: GoogleFonts.montserrat(
+        color: accentColor,
+        fontSize: 18.5,
+        fontWeight: FontWeight.w500,
+        fontStyle: FontStyle.italic,
+      ),
     ),
   );
 }
