5fb1710be8bceb6ff69ad06f231f00d0511bba7b
Gaby Nieva <gaby.nieva@outlook.com>
2024-02-21 13:58:52 -0600

    Initial widgets on Inputs Screen

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3_5c24/theme/app_theme.dart';
 
 class InputsScreen extends StatefulWidget {
   const InputsScreen({super.key});
@@ -10,8 +11,33 @@ class InputsScreen extends StatefulWidget {
 class _InputsScreenState extends State<InputsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Entradas'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Entradas',
+          style: AppTheme.lightTheme.textTheme.headlineLarge,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: null,
+              child: Text(
+                'Regresar',
+                style: AppTheme.lightTheme.textTheme.bodySmall,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text(
+                'Ir a Data Screen',
+                style: AppTheme.lightTheme.textTheme.bodySmall,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
