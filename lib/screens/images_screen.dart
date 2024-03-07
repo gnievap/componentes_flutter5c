15d3324844f12db4e40c03b6ac4cb97a8a6d0631
Gaby Nieva <gaby.nieva@outlook.com>
2024-03-07 11:16:04 -0600

    Images 3

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practica3_5c24/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -50,10 +51,29 @@ class _ImagesScreenState extends State<ImagesScreen> {
     );
   }
 
-  Widget imageWeb() {
-    return Center(
-      child: Image.network(
-          'https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:eco%2Cw_1200/MTk2Njc0ODUxNzAyODQzMTk4/colors-of-siberian-huskies.png'),
+  Stack imageWeb() {
+    return Stack(
+      children: <Widget>[
+        const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center(
+          child: SizedBox(
+            height: 450,
+            width: 450,
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image:
+                  'https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:eco%2Cw_1200/MTk2Njc0ODUxNzAyODQzMTk4/colors-of-siberian-huskies.png',
+            ),
+          ),
+
+          // Para mostrar una imagen de la web
+          // Image.network(
+
+          // ),
+        ),
+      ],
     );
   }
 }
